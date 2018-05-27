4d5333dc8b448d4f783936fb29849aafc58442b1
hhoomph <hh.oomph@gmail.com>
2018-05-27 05:14:25 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+app.get('/', (req, res) => {
+    res.send({
+        hi: 'there',
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
