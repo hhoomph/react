a97293d9c236203b39f8bf2f1e58ef77f4727bf1
hhoomph <38974044+hhoomph@users.noreply.github.com>
2018-05-28 05:21:57 +0430

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-    googleClientID: '207289414524-89bvfmgo61jv6qpcc8ftahjji8tdmvt5.apps.googleusercontent.com',
-    googleClientSecret: 'yn8pMDqhHK17DyKFuWMmnrer'
+    googleClientID: '207289414524-89bvfmgo61jv6qpcc8ftahjji8tdmvt5.apps.googleusercontent.com'
 };
