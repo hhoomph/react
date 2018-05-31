8a166e4884582e457fdfce71a1c7c741408fb78d
hhoomph <hh.oomph@gmail.com>
2018-05-31 05:57:21 +0430

    fixing .gitignore

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-// dev.js - Local envirement value, don't commit this!!
-module.exports = {
-    googleClientID: '207289414524-89bvfmgo61jv6qpcc8ftahjji8tdmvt5.apps.googleusercontent.com',
-    googleClientSecret: 'yn8pMDqhHK17DyKFuWMmnrer',
-    cookieKey: 'Vi&/@9keJm>k)v[%O%SC9eP)@8-6.W8Qk2ml^v+z?+=av{ud vYoGXr}|jJ~{^M-R|EYv&>$dL!'
-};
